355370d7776b51cbdba61781e441ab33fb4a5f03
Robert R. Kelley <rkelley@bellarmine.edu>
2024-04-14 19:10:56 -0400

    Finished first iteration of Streamlit App

--- visualize.py
+++ visualize.py
@@ -3,6 +3,8 @@ import streamlit as st
 import snowflake.connector
 from snowflake.connector import DictCursor
 import json
+import pandas as pd
+
 
 def get_connection(config_path='snowflake_config.json'):
     """Load configuration from the JSON file and establish connection 
@@ -21,49 +23,80 @@ def get_connection(config_path='snowflake_config.json'):
     )
     
     return conn
+
 def close_connection(cursor, conn):
-     # Always close cursor and connection
-        cursor.close()
-        conn.close()
-        
+    """CLoses both the shared cursor and connection to Snowflake"""
+    cursor.close()
+    conn.close()
+
+def get_pandas_data(query, conn):
+    df = pd.read_sql(query, conn)
+    return df
+      
 def get_data(query, cursor):
-    
+    """Execute the SQL query and fetch the data from Snowflake"""  #was cursor
     try:
+        #data=pd.read_sql(query, conn)
         # Execute SQL query
         cursor.execute(query)
         data = cursor.fetchall()  # Fetch all data
     finally:
-        pass
+        pass # Do nothing if an exception occurs
     
     return data
 
-# Main function to run the Streamlit app
+
 def main():
-    # Establish connection to Snowflake
+    """Main function to run the Streamlit app"""
+    #Establish connection to Snowflake
     conn = get_connection()
     
-     # Create a cursor object using a dictionary cursor to fetch rows as dictionaries
+    #Create a cursor object using a dictionary cursor to fetch rows as dictionaries
     cur = conn.cursor(DictCursor)
     
-    query = """SELECT
-                SYMBOL,
-                AVG(CLOSE_PRICE) AS "Average Close Price", 
-                MIN(CLOSE_PRICE) AS "Minimum Close Price",
-                MAX(CLOSE_PRICE) AS "Maximum Close Price" 
-            FROM
-                STOCK
-            GROUP BY SYMBOL
-            ORDER BY SYMBOL;
-            """
-    # Fetch data from Snowflake
-    data = get_data(query,cur)
-
-    # Set the title of the app
+     # Set the title of the app
     st.title('Invester Watch')
-
-    # Display the data as a table in Streamlit
-    st.write("Here are the average, minimum, and maximum close prices for each stock in your portfolio:")
-    st.table(data)
+    
+    summary_tab, query_tab, graph_tab = st.tabs(["Summary", "Query", "Graph"])
+    
+    with summary_tab:
+        # Display the data as a table in Streamlit
+        st.write("Here are the average, minimum, and maximum close prices for each stock in your portfolio:")
+    
+        #Establish the SQL query to fetch the average, minimum, and maximum close prices for each stock
+        query = """SELECT
+                    SYMBOL,
+                    AVG(CLOSE_PRICE) AS "Average Close Price", 
+                    MIN(CLOSE_PRICE) AS "Minimum Close Price",
+                    MAX(CLOSE_PRICE) AS "Maximum Close Price" 
+                FROM
+                    STOCK
+                GROUP BY SYMBOL
+                ORDER BY SYMBOL;
+                """
+        # Fetch data from Snowflake
+        data = get_data(query,cur)
+        st.table(data)
+    
+    with query_tab:
+        st.write("You can write your own SQL query to fetch data from the STOCK table.")
+        query = st.text_area("Enter your SQL query here:")
+        if st.button("Run Query"):
+            data = get_data(query, cur)
+            st.write(data)
+            
+    with graph_tab:
+        st.write("You can visualize the data from the STOCK table.")
+        query = """SELECT
+                    TICKER_DATE, CLOSE_PRICE
+                FROM
+                    STOCK
+                WHERE SYMBOL = 'APPL'
+                """
+        data = get_pandas_data(query, conn)
+        st.write(data.head())
+        st.line_chart(data.set_index('TICKER_DATE'))
+    
     
     close_connection(cur, conn)
 
