7cc1a4d85749e74b295dfbfcc38656568d184e0c
Robert R. Kelley <rkelley@bellarmine.edu>
2024-04-15 21:29:44 -0400

    Removed references to MySQL code and moved get_polygon_data to the utlitiles.py file.

--- scripts/daily_collector_snowflake.py
+++ scripts/daily_collector_snowflake.py
@@ -8,8 +8,8 @@ import snowflake.connector
 from snowflake.connector import DictCursor
 import utilities as util
 
-companies = ['META','AAPL','AMZN','NFLX','GOOG','MSFT', 'TSLA']  #company symbols to retrieve.
-stock_date = '2024-04-01'
+companies = ['AAPL','AMZN','GOOG','META','MSFT','NFLX', 'TSLA']  #company symbols to retrieve.
+stock_date = '2024-04-03'
 
 #Establish connection to Snowflake
 connection = util.get_snowflake_connection()
@@ -18,44 +18,11 @@ connection = util.get_snowflake_connection()
 global_cursor = connection.cursor(DictCursor)
 
 
-#You need to set up your connection with your credentials
-'''
-connection = mysql.connector.connect(user='stock_user', password='letmein',
-                              host='127.0.0.1',
-                              database='stock_data')
-global_cursor = connection.cursor()'''
 
-def get_company_data(ticker, timestamp, cnx, local_cursor):
-    request_string = 'https://api.polygon.io/v1/open-close/'+ticker+'/'+timestamp+'?adjusted=true&apiKey=46ipIrH5BrzkTqZj7OUvik7XAFOpu8GJ'
-    api_response = requests.get(request_string)
-    data = api_response.text
-    parse_json = json.loads(data)
-    db_row=[(v) for k, v in parse_json.items()]
-    
-    #for item in db_row:
-        #print(item)
-    
-    if(db_row[0] != 'NOT_FOUND'):
-    
-        symbol = db_row[2]
-        ticker_date = db_row[1]
-        open_price = db_row[3]
-        high_price = db_row[4]
-        low_price = db_row[5]
-        close_price = db_row[6]
-        volume = db_row[7]
 
-        sql = "INSERT INTO stock (symbol, ticker_date, open_price, high_price, low_price, close_price, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (symbol, ticker_date, open_price, high_price, low_price, close_price, volume)
-        
-        local_cursor.execute(sql, val)
-        cnx.commit()
-
-        print(local_cursor.rowcount, "record inserted.")
-        time.sleep(15)
         
 for company in companies:
-    get_company_data(company, stock_date ,connection, global_cursor)
+    util.get_polygon_data(company, stock_date ,connection, global_cursor)
     
 global_cursor.close()
 connection.close()
--- scripts/utilities.py
+++ scripts/utilities.py
@@ -8,7 +8,7 @@ import time
 import snowflake.connector
 from snowflake.connector import DictCursor
 
-def get_company_data(ticker, start_day, end_day):
+def get_bulk_stock_data(ticker, start_day, end_day):
     '''Uses the Polygon API to get stock data for a given ticker symbol and date range.'''
     request_string='https://api.polygon.io/v2/aggs/ticker/'+ticker+'/range/1/day/'+start_day+'/'+end_day+'?adjusted=true&sort=asc&apiKey=46ipIrH5BrzkTqZj7OUvik7XAFOpu8GJ'
 
@@ -66,3 +66,33 @@ def close_snowflake_connection(cursor, conn):
     """CLoses both the shared cursor and connection to Snowflake"""
     cursor.close()
     conn.close()
+    
+    
+def get_polygon_data(ticker, timestamp, cnx, local_cursor):
+    request_string = 'https://api.polygon.io/v1/open-close/'+ticker+'/'+timestamp+'?adjusted=true&apiKey=46ipIrH5BrzkTqZj7OUvik7XAFOpu8GJ'
+    api_response = requests.get(request_string)
+    data = api_response.text
+    parse_json = json.loads(data)
+    db_row=[(v) for k, v in parse_json.items()]
+    
+    #for item in db_row:
+        #print(item)
+    
+    if(db_row[0] != 'NOT_FOUND'):
+    
+        symbol = db_row[2]
+        ticker_date = db_row[1]
+        open_price = db_row[3]
+        high_price = db_row[4]
+        low_price = db_row[5]
+        close_price = db_row[6]
+        volume = db_row[7]
+
+        sql = "INSERT INTO stock (symbol, ticker_date, open_price, high_price, low_price, close_price, volume) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (symbol, ticker_date, open_price, high_price, low_price, close_price, volume)
+        
+        local_cursor.execute(sql, val)
+        cnx.commit()
+
+        print(local_cursor.rowcount, f"record inserted for {ticker} on {timestamp}")
+        time.sleep(15)
